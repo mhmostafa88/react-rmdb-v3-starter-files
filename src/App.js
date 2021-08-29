848eafd2aa3354868a18ece249bedd65c2e0ad51
codingwizard88 <coding_wizard@outlook.com>
2021-08-29 09:20:39 +0300

    laying the groundwork for components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-function App() {
+// Components
+import Header from './components/Header';
+
+// Styles
+import { GlobalStyle } from './GlobalStyle';
+
+const App = () => {
   return (
     <div className="App">
+    <Header />
       Start here.
+      
+      <GlobalStyle />
     </div>
   );
 }
