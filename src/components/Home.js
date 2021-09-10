b41f8b41903555429beab3162bc0a7b0ef1dcdb3
codingwizard88 <coding_wizard@outlook.com>
2021-09-10 09:45:42 +0300

    spinner and search bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,8 @@ import { POSTER_SIZE , BACKDROP_SIZE, IMAGE_BASE_URL  } from '../config';
 import HeroImage from './HeroImage';
 import Grid from './Grid';
 import Thumb from './Thumb';
+import Spinner from './Spinner';
+import SearchBar from './SearchBar';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -16,7 +18,7 @@ import NoImage from '../images/no_image.jpg'
 
 const Home =() => {
     
-    const { state, loading, error } = useHomeFetch();
+    const { state, loading, error, setSearchTerm } = useHomeFetch();
     return (
     <>
         {state.results[0] && 
@@ -26,6 +28,7 @@ const Home =() => {
             text={state.results[0].overview}
         />
         }
+        <SearchBar setSearchTerm={setSearchTerm}/>
         <Grid header='Popular Movies'>
             {state.results.map(movie => (
                 <Thumb 
